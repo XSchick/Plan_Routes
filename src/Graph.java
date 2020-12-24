076688fdb23b1ec8ed3d9461b6cab2302c83d964
Xaver Schick <Xaver.schick@web.de>
2020-12-24 02:23:41 +0100

    Finishing all functions

--- src/Graph.java
+++ src/Graph.java
@@ -1,15 +1,13 @@
-import com.sun.jdi.ArrayReference;
 import edu.kit.informatik.Terminal;
 
-import java.util.Arrays;
-
 public class Graph {
-    private String FilePath;
+    private String filePath;
     private int startingCity;
     private int targetCity;
     private int lengthOfPath;
-    private int[][] matrixA;
     private String[] serialization;
+    private int[][] pathMatrix;
+    private int[][] multipliedMatrix;
 
     /**
      * Constructor
@@ -20,44 +18,66 @@ public class Graph {
      * @param theLengthOfPath length of path
      */
     public Graph(String theFilePath, String theStartingCity, String theTargetCity, String theLengthOfPath) {
-        this.FilePath = theFilePath;
+        this.filePath = theFilePath;
         this.startingCity = Integer.parseInt(theStartingCity);
         this.targetCity = Integer.parseInt(theTargetCity);
         this.lengthOfPath = Integer.parseInt(theLengthOfPath);
-        this.serialization = Terminal.readFile(this.FilePath);
+        this.serialization = Terminal.readFile(this.filePath);
     }
 
+    /**
+     * Checks how many cities are used
+     *
+     * @return number of cities
+     */
     private int determineNumberOfCities() {
         int max = 0;
         for (String value : serialization) {
             int a = Integer.parseInt(String.valueOf(value.charAt(0)));
+            int b = Integer.parseInt(String.valueOf(value.charAt(2)));
             if (max < a) {
                 max = a;
             }
-        }
-        for (String value : serialization) {
-            int b = Integer.parseInt(String.valueOf(value.charAt(2)));
             if (max < b) {
                 max = b;
             }
         }
-        return max + 1;
+        return ++max;
     }
 
-    public void initializeMatrix() {
-        this.matrixA = new int[determineNumberOfCities()][determineNumberOfCities()];
-
+    /**
+     * Creates an adjacency matrix from the connections between the cities,
+     * which are read from the text file
+     */
+    public void createAdjacencyMatrix() {
+        //Initialize adjacency matrix
+        this.pathMatrix = new int[determineNumberOfCities()][determineNumberOfCities()];
+        //Set all fields to zero
         for (int i = 0; i < determineNumberOfCities(); i++) {
             for (int x = 0; x < determineNumberOfCities(); x++) {
-                this.matrixA[i][x] = 0;
+                this.pathMatrix[i][x] = 0;
             }
         }
+        for (int i = 0; i < serialization.length; i++) {
+            this.pathMatrix[getSourceCity(serialization[i])][getTargetCity(serialization[i])] = 1;
+        }
+        multipliedMatrix = pathMatrix;
     }
 
-    private void writeMatrix() {
-        initializeMatrix();
-        for (int i = 0; i < serialization.length; i++) {
-            this.matrixA[getSourceCity(serialization[i])][getTargetCity(serialization[i])] = 1;
+    /**
+     * Multiplies a matrix by the path matrix, which is creates in method createAdjacencyMatrix()
+     *
+     * @param matrix The matrix by which the path matrix is multiplied
+     */
+    private void multiplyMatrix(int[][] matrix) {
+        this.multipliedMatrix = new int[determineNumberOfCities()][determineNumberOfCities()];
+        for (int i = 0; i < determineNumberOfCities(); i++) {
+            for (int j = 0; j < determineNumberOfCities(); j++) {
+                this.multipliedMatrix[i][j] = 0;
+                for (int k = 0; k < determineNumberOfCities(); k++) {
+                    this.multipliedMatrix[i][j] += matrix[i][k] * pathMatrix[k][j];
+                }
+            }
         }
     }
 
@@ -69,10 +89,16 @@ public class Graph {
         return Integer.parseInt(String.valueOf(line.charAt(2)));
     }
 
+    /**
+     * Calls the responsible methods to create the matrices and outputs the result at the end
+     */
     public void output() {
-        writeMatrix();
-        System.out.println();
-        //System.out.println(this.matrixA[1][1]);
+        createAdjacencyMatrix();
+        for (int i = 1; i < this.lengthOfPath; i++) {
+            multiplyMatrix(this.multipliedMatrix);
+        }
+        Terminal.printLine(multipliedMatrix[startingCity][targetCity]);
+
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import edu.kit.informatik.Terminal;
-
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(args[0], args[1], args[2], args[3]);
